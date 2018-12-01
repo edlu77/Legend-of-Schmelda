605447e1c4884e98906ae30f3c2ebe0658a5b245
edlu77 <eddd.lu@gmail.com>
2018-12-01 00:30:49 -0500

    refactor link into class

--- dist/bundle.js
+++ dist/bundle.js
@@ -81,32 +81,10 @@
 /******/
 /******/
 /******/ 	// Load entry module and return exports
-/******/ 	return __webpack_require__(__webpack_require__.s = "./lib/game.js");
+/******/ 	return __webpack_require__(__webpack_require__.s = "./lib/schmelda.js");
 /******/ })
 /************************************************************************/
 /******/ ({
-
-/***/ "./lib/background.js":
-/*!***************************!*\
-  !*** ./lib/background.js ***!
-  \***************************/
-/*! exports provided: default */
-/***/ (function(module, __webpack_exports__, __webpack_require__) {
-
-"use strict";
-__webpack_require__.r(__webpack_exports__);
-class Background {
-  constructor() {
-
-  }
-
-  
-}
-
-/* harmony default export */ __webpack_exports__["default"] = (Background);
-
-
-/***/ }),
 
 /***/ "./lib/enemy.js":
 /*!**********************!*\
@@ -156,28 +134,300 @@ class Enemy {
 
 "use strict";
 __webpack_require__.r(__webpack_exports__);
-/* harmony import */ var _enemy__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./enemy */ "./lib/enemy.js");
-/* harmony import */ var _background__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./background */ "./lib/background.js");
+/* harmony import */ var _enemy_js__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./enemy.js */ "./lib/enemy.js");
+/* harmony import */ var _link_js__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./link.js */ "./lib/link.js");
 
 
 
 class Game {
-  constructor() {
-    this.enemies = [];
-    this.background = new _background__WEBPACK_IMPORTED_MODULE_1__["default"]();
-    this.canvas = document.getElementById('canvas');
-    this.ctx = canvas.getContext('2d');
-    this.enemy = new _enemy__WEBPACK_IMPORTED_MODULE_0__["default"]();
+  constructor(canvas, ctx) {
+    this.enemy = new _enemy_js__WEBPACK_IMPORTED_MODULE_0__["default"]();
+    this.link = new _link_js__WEBPACK_IMPORTED_MODULE_1__["default"](canvas, ctx);
+    this.ctx = ctx;
+    this.canvas = canvas;
+    this.draw = this.draw.bind(this)
   }
 
   draw() {
-    enemy.draw
-
+    // this.enemy.draw(this.ctx)
+    this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height)
+    this.ctx.beginPath();
+    this.ctx.lineWidth="6";
+    this.ctx.strokeStyle="red";
+    this.ctx.rect(5,5,290,140);
+    this.ctx.stroke();
+    this.link.draw();
   }
 
 }
 
 /* harmony default export */ __webpack_exports__["default"] = (Game);
+
+
+/***/ }),
+
+/***/ "./lib/link.js":
+/*!*********************!*\
+  !*** ./lib/link.js ***!
+  \*********************/
+/*! exports provided: default */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+const directions = ["walkRight", "walkLeft", "walkDown", "walkUp"]
+
+const dir_hash_y = {
+  "walkRight": 120,
+  "walkLeft": 30,
+  "walkDown": 30,
+  "walkUp": 120,
+}
+
+const dir_hash_x = {
+  "walkRight": [241, 271, 301, 331, 361, 391],
+  "walkLeft": [241, 271, 301, 331, 361, 391],
+  "walkDown": [0, 30, 60, 90, 120, 150, 180, 210],
+  "walkUp": [0, 30, 60, 90, 120, 150, 180, 210],
+}
+
+const standing = [
+  [331, 120],
+  [151, 0],
+  [31, 0],
+  [61, 0],
+] //right left down up
+
+class Link {
+  constructor(canvas, ctx) {
+    this.canvas = canvas;
+    this.ctx = ctx;
+    this.link = new Image();
+    this.link.src = './assets/link_sprites.png';
+    this.width = 20;
+    this.height = 25;
+    this.scale = 4;
+    this.scaledWidth = this.scale*this.width;
+    this.scaledHeight = this.scale*this.height;
+    this.currentLoopIndex = 0;
+    this.currentDirection = 0;
+    this.canvasX = 0;
+    this.canvasY = 0;
+    this.frameCount = 0;
+
+    this.step = this.step.bind(this)
+    this.move = this.move.bind(this)
+  }
+
+  drawFrame(direction, frameX, frameY, canvasX, canvasY) {
+    this.ctx.clearRect(0, 0, this.canvas.width, this.canvas.height)
+    this.ctx.drawImage(
+      this.link,
+      dir_hash_x[direction][frameX],
+      dir_hash_y[direction],
+      this.width,
+      this.height,
+      canvasX,
+      canvasY,
+      this.scaledWidth,
+      this.scaledHeight,
+    )
+  }
+
+  step() {
+    let numFrames = dir_hash_x[directions[this.currentDirection]].length
+    let cycleLoop = Array.from({length: numFrames}, (x,i) => i);
+    this.frameCount++
+    if (this.frameCount < 5) {
+      window.requestAnimationFrame(this.step);
+      return;
+    }
+    this.frameCount = 0;
+    this.drawFrame(
+      directions[this.currentDirection],
+      cycleLoop[this.currentLoopIndex],
+      dir_hash_y[directions[this.currentDirection]],
+      this.canvasX,
+      this.canvasY,
+      80,
+      100
+    );
+
+    this.currentLoopIndex++;
+    if (this.currentLoopIndex >= cycleLoop.length) {
+      this.currentLoopIndex = 0;
+    }
+    window.requestAnimationFrame(this.step);
+  }
+
+  draw() {
+    this.step()
+  }
+
+  move(e) {
+    if (e.key === "ArrowLeft") {
+      this.canvasX -= 20;
+      this.currentDirection = 1;
+    } else if (e.key === "ArrowRight") {
+      this.canvasX += 20;
+      this.currentDirection = 0;
+    } else if (e.key === "ArrowDown") {
+      this.canvasY += 20;
+      this.currentDirection = 2;
+    } else if (e.key === "ArrowUp") {
+      this.canvasY -= 20;
+      this.currentDirection = 3;
+    } else {
+      return
+    }
+    // step()
+  }
+
+// stand(e) {
+//     moving = false
+//     ctx.clearRect(0, 0, canvas.width, canvas.height)
+//     ctx.drawImage(
+//       link, standing[currentDirection][0], standing[currentDirection][1], width, height, canvasX, canvasY, scaledWidth, scaledHeight
+//     );
+//   }
+
+}
+
+/* harmony default export */ __webpack_exports__["default"] = (Link);
+
+// --------------------------------------
+// let link = new Image();
+// link.src = './assets/link_sprites.png';
+// link.onload = function() {
+//   init()
+// };
+//
+//
+// const directions = ["walkRight", "walkLeft", "walkDown", "walkUp"]
+//
+// const dir_hash_y = {
+//   "walkRight": 120,
+//   "walkLeft": 30,
+//   "walkDown": 30,
+//   "walkUp": 120,
+// }
+//
+// const dir_hash_x = {
+//   "walkRight": [241, 271, 301, 331, 361, 391],
+//   "walkLeft": [241, 271, 301, 331, 361, 391],
+//   "walkDown": [0, 30, 60, 90, 120, 150, 180, 210],
+//   "walkUp": [0, 30, 60, 90, 120, 150, 180, 210],
+// }
+//
+// const standing = [
+//   [331, 120],
+//   [151, 0],
+//   [31, 0],
+//   [61, 0],
+// ] //right left down up
+//
+// const canvas = document.getElementById('canvas');
+// const ctx = canvas.getContext('2d');
+// const width = 20;
+// const height = 25;
+// const scale = 4;
+// const scaledWidth = scale*width;
+// const scaledHeight = scale*height;
+// const offset = 30;
+//
+// function drawFrame(direction, frameX, frameY, canvasX, canvasY) {
+//   ctx.drawImage(
+//     link, dir_hash_x[direction][frameX], dir_hash_y[direction], width, height, canvasX, canvasY, scaledWidth, scaledHeight);
+// }
+//
+// let currentLoopIndex = 0;
+// let frameCount = 0;
+// let currentDirection = 0;
+// let canvasX = 0;
+// let canvasY = 0;
+// let moving = false
+//
+// function init() {
+//   step()
+// }
+//
+// function step() {
+//   if (!moving) {
+//     stand()
+//     return
+//   }
+//
+//   let numFrames = dir_hash_x[directions[currentDirection]].length
+//   let cycleLoop = Array.from({length: numFrames}, (x,i) => i);
+//   frameCount ++
+//   if (frameCount < 4) {
+//     window.requestAnimationFrame(step);
+//     return;
+//   }
+//   frameCount = 0;
+//   ctx.clearRect(0, 0, canvas.width, canvas.height)
+//   drawFrame(directions[currentDirection], cycleLoop[currentLoopIndex], dir_hash_y[directions[currentDirection]], canvasX, canvasY, 80, 100);
+//   currentLoopIndex++;
+//   if (currentLoopIndex >= cycleLoop.length) {
+//     currentLoopIndex = 0;
+//   }
+//   window.requestAnimationFrame(step);
+// }
+//
+// function move(e) {
+//   moving = true
+//   if (e.key === "ArrowLeft") {
+//     canvasX -= 25;
+//     currentDirection = 1;
+//   } else if (e.key === "ArrowRight") {
+//     canvasX += 25;
+//     currentDirection = 0;
+//   } else if (e.key === "ArrowDown") {
+//     canvasY += 25;
+//     currentDirection = 2;
+//   } else if (e.key === "ArrowUp") {
+//     canvasY -= 25;
+//     currentDirection = 3;
+//   } else {
+//     return
+//   }
+//   step()
+// }
+//
+// function stand(e) {
+//   moving = false
+//   ctx.clearRect(0, 0, canvas.width, canvas.height)
+//   ctx.drawImage(
+//     link, standing[currentDirection][0], standing[currentDirection][1], width, height, canvasX, canvasY, scaledWidth, scaledHeight
+//   );
+// }
+//
+
+// document.onkeydown = this.link.move;
+// document.onkeyup = this.stand;
+
+
+/***/ }),
+
+/***/ "./lib/schmelda.js":
+/*!*************************!*\
+  !*** ./lib/schmelda.js ***!
+  \*************************/
+/*! no exports provided */
+/***/ (function(module, __webpack_exports__, __webpack_require__) {
+
+"use strict";
+__webpack_require__.r(__webpack_exports__);
+/* harmony import */ var _game_js__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./game.js */ "./lib/game.js");
+
+
+document.addEventListener('DOMContentLoaded', ()=> {
+  const mainCanvas = document.getElementById('main-window');
+  const ctx = mainCanvas.getContext('2d');
+  const game = new _game_js__WEBPACK_IMPORTED_MODULE_0__["default"](mainCanvas, ctx)
+  game.draw();
+  document.onkeydown = game.link.move;
+});
 
 
 /***/ })
